125f32c63b2a6e8a486a2fb6de6f7fe6f4edcfde
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-17 18:58:00 -0800

    GClock_V1.java
    An archive of the first version of the Google Clock alternative Java program.

---
+++ Standard_Apps/JAVA/Clock/OldVersions/GClock_V1.java
@@ -0,0 +1,129 @@
+// Start of script
+/* Start of import section */
+// Custom libraries used (2x) not yet available
+// import candroid.api;
+// import candroid.apilvl;
+import Java.util.Scanner;
+/* End of import section */
+public static void main() {
+  // Perform an API check first to display the correct version of the clock app
+  System.out.println("Google clock for Candroid");
+  System.out.println("An open source alternate for the Android clock application");
+  System.out.println("\nThis program is incomplete. An API level cannot be fetched. This program is not functional yet");
+  break;
+}
+public void alphaClock() {
+  // The clock for Android 1.0
+  System.out.println("Android 1.0 clock app for Candroid\nNot yet available");
+  break;
+}
+public void petitClock() {
+  // The clock for Android 1.1
+  System.out.println("Android 1.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void cupcakeClock() {
+  // The clock for Android 1.5
+  System.out.println("Android 1.5 clock app for Candroid\nNot yet available");
+  break;
+}
+public void donutClock() {
+  // The clock for Android 1.6
+  System.out.println("Android 1.6 clock app for Candroid\nNot yet available");
+  break;
+}
+public void eclairClock() {
+  // The clock for Android 2.0
+  System.out.println("Android 2.0 clock app for Candroid\nNot yet available");
+  break;
+}
+public void froyoClock() {
+  // The clock for Android 2.1
+  System.out.println("Android 2.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void gingerbreadClock() {
+  // The clock for Android 2.2 and 2.3
+  System.out.println("Android 2.2 clock app for Candroid\nNot yet available");
+  System.out.println("Android 2.3 clock app for Candroid\nNot yet available");
+  break;
+}
+public void honeycombClock() {
+  // The clock for Android 3.0 and 3.1
+  System.out.println("Android 3.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 3.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void iceCreamSandwichClock() {
+  // The clock for Android 4.0 and 4.1
+  System.out.println("Android 4.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 4.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void jellyBeanClock() {
+  // The clock for Android 4.2 and 4.3
+  System.out.println("Android 4.2 clock app for Candroid\nNot yet available");
+  System.out.println("Android 4.3 clock app for Candroid\nNot yet available");
+  break;
+}
+public void kitClock() {
+  // The clock for Android 4.4
+  System.out.println("Android 4.4 clock app for Candroid\nNot yet available");
+  break;
+}
+public void lollipopClock() {
+  // The clock for Android 5
+  System.out.println("Android 5.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 5.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void marshmallowClock() {
+  // The clock for Android 6
+  System.out.println("Android 6.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 6.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void nougatClock() {
+  // The clock for Android 7
+  System.out.println("Android 7.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 7.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void oreoClock() {
+  // The clock for Android 8
+  System.out.println("Android 8.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 8.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void pieClock() {
+  // The clock for Android 9
+  System.out.println("Android 9.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 9.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void q10Clock() {
+  // The clock for Android 10
+  System.out.println("Android 10.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 10.1 clock app for Candroid\nNot yet available");
+  break;
+}
+public void r11Clock() {
+  // The clock for Android 11
+  System.out.println("Android 11.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 11.1 clock app for Candroid\nNot yet available");
+  break;
+}
+/* 
+public void s12Clock() {
+  // The clock for Android 12
+  System.out.println("Android 12.0 clock app for Candroid\nNot yet available");
+  System.out.println("Android 12.1 clock app for Candroid\nNot yet available");
+  break;
+}
+*/
+/* File info
+* File type: Java source file (*.java)
+* File version: 1 (Sunday, January 17th 2021 at 6:57 pm)
+* Line count (including blank lines and compiler line): 130
+*/
+// End of script
